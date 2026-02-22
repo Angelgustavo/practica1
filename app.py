3d15d5b7bff065c539650a4a2fba0ca08ccae9e4
Angel Toapanta <atoapanta81@gmail.com>
2026-02-22 17:02:42 -0500

    Carga

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 @app.route('/usuarios', methods=['GET'])# consultar usuarios 
 def obtener_usuarios():
     try:
+        #cambio
         conn = obtener_conexion()
         usuarios = conn.execute("SELECT * FROM usuarios").fetchall()
         conn.close()
